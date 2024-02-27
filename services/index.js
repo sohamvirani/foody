63b347ce03e8ba1539299272ded9c7cc3ef8ccc5
sohamvirani <141616387+sohamvirani@users.noreply.github.com>
2024-02-27 09:09:14 +0530

    Add files via upload

---
+++ services/index.js
@@ -0,0 +1,5 @@
+module.exports.menuService = require("./menu.service");
+module.exports.orderService = require("./order.service");
+module.exports.paymentService = require("./payment.service");
+module.exports.restaruntService = require("./restarunt.service");
+module.exports.userService = require("./user.service");
